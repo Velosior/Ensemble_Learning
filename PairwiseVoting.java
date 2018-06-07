80f2e3928b797b32866180d04783352efa9c6411
Walter Scott <walter-scott@hotmail.com>
2018-06-07 20:54:03 +1200

    Added options to the class.

--- PairwiseVoting.java
+++ PairwiseVoting.java
@@ -6,9 +6,8 @@ import moa.core.DoubleVector;
 import moa.core.Measurement;
 import moa.core.ObjectRepository;
 import moa.options.ClassOption;
-import moa.options.FlagOption;
-import moa.options.FloatOption;
-import moa.options.ListOption;
+import moa.options.IntOption;
+import moa.options.MultiChoiceOption;
 import moa.options.Option;
 import moa.tasks.TaskMonitor;
 import weka.core.Instance;
@@ -22,5 +21,16 @@ public class PairwiseVoting extends AbstractClassifier
 		return "Pairwise voting algorithm for data streams.";
 	}
 	
+	public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l', "Classifier to train.", Classifier.class, "trees.HoeffdingTree");
+
+	public IntOption ensembleSizeOption = new IntOption("ensembleSize", 's', "The number of models to use in voting.", 10, 1, Integer.MAX_VALUE);
 	
+	public MultiChoiceOption votingAlgorithmOption = new MultiChoiceOption(
+		"votingAlgorithm", 'm', "Voting strategy to use.", new String[]{
+			"PairwiseAccuracy", "PairwisePatterns"},
+		new String[]{"Weights predictions of pairs based on their shared estimated accuracy.",
+			"Uses recorded prediction patterns to weight decisions while predicting an unknown instance."
+			}, 0);
+	
+	protected Classifier[] ensemble;
 }
